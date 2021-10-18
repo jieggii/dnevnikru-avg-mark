0c3045f664f2e1457c0b629a2adb177c2996477b
jieggii <jieggii.contact@gmail.com>
2021-10-18 14:44:05 +0300

    Add dnevnik.ru style marks coloring

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 function parseMarks(string) {
     string = string.replace(/\D/g, "");
-    let rawMarks = string.split("");
-    return rawMarks.map((item) => parseInt(item))
+    let stringMarks = string.split("");
+    return stringMarks.map((item) => parseInt(item))
 }
 
 function countAverage(array) {
     return array.reduce((a, b) => a + b, 0) / array.length;
 }
 
+function generateMarkClass(mark) {
+    if (mark >= 4) {
+        return "mG"
+    } else if (mark >= 3) {
+        return "mY"
+    } else {
+        return "mR"
+    }
+}
+
 function insertCell(row, content) {
     row.insertCell().outerHTML = "<td>" + content + "</td>";
+}
+
+function insertMarkCell(row, mark) {
+    let mark_class = generateMarkClass(mark);
+    insertCell(row, "<span class='mark " + mark_class + "'>" + mark + "</span>");
 }
 
 let table = document.getElementById("journal");
@@ -17,10 +32,10 @@ table.rows[0].insertCell().outerHTML = "<th rowspan=2>Среднее арифм�
 
 for (let row of [...table.rows].slice(2)) {
     let rawMarks = row.cells[2].textContent;
-    if (rawMarks) {
+    if (rawMarks !== "") {
         let marks = parseMarks(rawMarks);
         let average = countAverage(marks);
-        insertCell(row, Math.round(average*100) / 100);
+        insertMarkCell(row, Math.round(average * 100) / 100);
     } else {
         insertCell(row, "");
     }
